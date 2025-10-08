0c8b94015c817211b5d9e008d510248a42d736fc
Mian Ali <itsaaliraza@gmail.com>
2025-10-08 08:43:53 +0500

    updated code

--- download.py
+++ download.py
@@ -153,4 +153,4 @@ async def api_download(request: VideoRequest, background: BackgroundTasks):
 
 # --- Run Server ---
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=1000)
+    uvicorn.run(app, host="0.0.0.0", port=1000)
